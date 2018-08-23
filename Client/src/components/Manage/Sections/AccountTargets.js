9a3ca84e38914ddc9cb3beab98a52235f3ed6aa0
joneslee0918 <joneslee0918@gmail.com>
2018-08-23 17:48:09 +0200

    Configurable UserList

--- Client/src/components/Manage/Sections/AccountTargets.js
+++ Client/src/components/Manage/Sections/AccountTargets.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import UserList from "../../UserList";
 
+const userItems = [
+    {   
+        id: 1,
+        name: "Albert Feka",
+        username: "@anime.masters89",
+        title: "Veteran i luftes",
+        tweets: 200,
+        followers: 380,
+        following: 450
+    },
+    {   
+        id: 2,
+        name: "Albert Feka",
+        username: "@anime.masters89",
+        title: "Veteran i luftes",
+        tweets: 200,
+        followers: 380,
+        following: 450
+    },
+    {  
+        id: 3,
+        name: "Albert Feka",
+        username: "@anime.masters89",
+        title: "Veteran i luftes",
+        tweets: 200,
+        followers: 380,
+        following: 450
+    }
+];
 const AccountTargets = () => (
     <div>
-        <h3>AccountTargets</h3>
+        <h2>ACCOUNT TARGETS</h2>
+        <UserList 
+            userItems={ userItems }
+            actionType="follow"
+            showTargetLink={true}
+            targetType="account"
+        />
     </div>
 );
 
--- Client/src/components/UserList.js
+++ Client/src/components/UserList.js
@@ -1,7 +1,109 @@
 import React from 'react';
 
-const UserList = () => (
-    <div> This is the user list. </div>
+const UserList = (
+        {   
+            userItems = [],
+            showTargetLink = false,
+            targetType = "account", 
+            showSortOption = false,
+            actionType = "follow"
+        }
+    ) => { 
+    
+    const actionButton = actionType === "follow" ? "add" : "sub";
+
+    return (
+        <div>
+
+            <ListActions actionType={ actionType }/>
+
+            {
+                userItems.length < 1 ? 
+                            
+                <div class="no-data">
+                    Nothing to do. <br/>Please check back later! <br /> :)
+                </div> :
+
+                <div className="row">
+                    <div className="col-xs-12">
+                        <div className="item-list shadow-box">
+                            <div className="item-header">
+                                { showTargetLink &&  <TargetsLink targetType={ targetType }/> }
+                                { showSortOption && <SortOption /> }
+                            </div>
+
+                            {userItems.map((item) => (
+                                <UserItem key={item.id} userItem={ item } actionButton={ actionButton } />
+                            ))}
+                            
+                        </div>
+                    </div>
+                </div> 
+            }
+
+        </div>
+    );
+}
+
+const ListActions = ({ actionType }) => (
+    <div className="action-count pull-right">
+        <p><span className="action-count-title">{ actionType }s</span> : <span className={`${actionType}-count daily-actions`}>23</span></p>
+    </div>
 );
+
+const TargetsLink = ({ targetType }) => (
+    <div className="pull-left">
+        Showing list of accounts based on your <a href="#" >{ targetType } targets <i className="fa fa-pencil"></i></a>
+    </div>
+);
+
+const SortOption = () => (
+    <div className="pull-right form-inline sort-options">
+    <label className="sortLabel" htmlFor="sort">Sort by: </label>
+
+        <form action="#" className="form-group" id="order_form">
+            <select name="order" id="order">
+                <option value="desc" defaultValue >Newest First</option>
+                <option value="asc">Oldest First</option>
+            </select>
+        </form>
+
+    </div>
+);
+
+const UserItem = ({ userItem, actionButton }) => (
+    <div className="item-row">
+        <div className="profile-info pull-left">
+            <img className="pull-left" src="" />
+            <div className="pull-left">
+                <input type="hidden" className="user_id" value=""/>
+                <p className="profile-name">{ userItem.name } <span className="profile-username">{ userItem.username }</span></p>
+                <p className="profile-title">{ userItem.title }</p>
+                <ul className="bottom-info">
+                    <li><p>{ userItem.tweets }</p></li>
+                    <li><p>{ userItem.followers }</p></li>
+                    <li><p>{ userItem.following }</p></li>
+                </ul>
+            </div>
+        </div>
+
+        <UserActionButtons actionButton={ actionButton } />
+    </div>
+);
+
+const UserActionButtons = ({ actionButton }) => {
+
+    const actionSymbol = actionButton === "add" ? "plus" : "minus";
+
+    return (
+        <div className="item-actions pull-right">
+            <ul>
+                <li className="text-links"><a href="#">Reply</a></li>
+                <li className="text-links"><a href="#">Whitelist</a></li>
+                <li className="btn-links"><div data-user-id="" className={`${actionButton}-btn action-btn`}><i className={`fa fa-${actionSymbol}-circle`}></i></div></li>
+            </ul>
+        </div>
+    );
+}
 
 export default UserList;
