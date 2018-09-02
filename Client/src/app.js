798e78dd56a84de219d016c1c334284cebf6dda4
joneslee0918 <joneslee0918@gmail.com>
2018-09-02 23:07:54 +0200

    fixed refresh bug

--- Client/src/app.js
+++ Client/src/app.js
@@ -11,28 +11,37 @@ import { setChannels } from "./actions/channels";
 
 const store = configStore();
 
-class Root extends React.Component {
-    componentDidMount = () => {
-        const token = localStorage.getItem("token") || undefined;
-        store.dispatch(login(token));
-        setAuthorizationHeader(token);
+const Root = () => (
+    <div>
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>    
+    </div>
+);
 
-        if(token){
-            let channels = localStorage.getItem("channels");
-            channels = channels ? JSON.parse(channels) : [];
-            store.dispatch(setChannels(channels));
-        }
-    };
 
-    render(){
-        return (
-            <div>
-                <Provider store={store}>
-                    <AppRouter />
-                </Provider>    
-            </div>
-        );
+let hasRendered = false;
+
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(<Root />, document.getElementById("app"));
+        hasRendered = true;
     }
-}
+};
 
-ReactDOM.render(<Root />, document.getElementById("app"));
+
+const setAuthentication = () => {
+    const token = localStorage.getItem("token") || undefined;
+    store.dispatch(login(token));
+    setAuthorizationHeader(token);
+
+    if(token){
+        let channels = localStorage.getItem("channels");
+        channels = channels ? JSON.parse(channels) : [];
+        store.dispatch(setChannels(channels));
+    }
+
+    renderApp();
+};
+
+setAuthentication();
