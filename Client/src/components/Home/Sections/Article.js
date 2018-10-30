8ec0653b804ffe4af37d8263c65d86f6b0d17b7d
joneslee0918 <joneslee0918@gmail.com>
2018-10-30 10:20:15 +0100

    Hyperlinked articles

--- Client/src/components/Home/Sections/Article.js
+++ Client/src/components/Home/Sections/Article.js
@@ -4,11 +4,15 @@ const Article = ({article}) => (
 
     <div className="card-display">
         <div className="card-content">
-            <img className="card-img-top" onError={(e) => e.target.src='/images/no-image-box.png'} src={article.image_url} />
+            <a target="_blank" href={article.url}>
+                <img className="card-img-top" onError={(e) => e.target.src='/images/no-image-box.png'} src={article.image_url} />
+            </a>
             <div className="card-body">
-                <h5 className="card-title">{article.title}</h5>
-                <p className="card-text">{article.description}</p>
-                <p className="card-text"><small className="text-muted">{article.source_url}</small></p>
+                <a target="_blank" href={article.url}>
+                    <h5 className="card-title">{article.title}</h5>
+                    <p className="card-text">{article.description}</p>
+                    <p className="card-text"><small className="text-muted">{article.source_url}</small></p>
+                </a>
                 <div className="center-inline">
                     <button className="upgrade-btn">Share</button>
                 </div>
