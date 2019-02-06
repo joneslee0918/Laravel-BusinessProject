4949afdc07077525734c03839383c34c26c3def1
joneslee0918 <joneslee0918@gmail.com>
2019-02-06 16:25:06 +0100

    Streams- add/remove tabs front-end

--- Client/src/components/Streams/Tabs.js
+++ Client/src/components/Streams/Tabs.js
@@ -27,17 +27,51 @@ class Tabs extends React.Component{
         ]
     }
 
+    newTab = () => {
+        
+        const tabs = this.state.tabs.map( tab => {
+            return {
+                name: tab.name,
+                id: tab.id,
+                active:false
+            }
+        });
+
+        this.setState(() => ({
+            tabs: [
+                ...tabs,
+                {
+                    name: `Tab ${(tabs.length + 1)}`,
+                    id: `tab_${(tabs.length + 1)}`,
+                    active: true
+                }
+            ]
+        }));
+    };
+
+    removeTab = (id) => {
+
+        const tabs = this.state.tabs.filter( tab => tab.id !== id );
+
+        this.setState(() => ({
+            tabs
+        }));
+    }
+
+    
+
     render(){
         return(
             <div>
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                     {this.state.tabs.map( (tab, index) => (
-                        <li key={index} className={`nav-item ${tab.active ? 'active' : ''}`}>
-                            <a className={`nav-link bordered-tab ${tab.active ? 'active' : ''}`} id={`${tab.id}-tab`} data-toggle="tab" href={`#${tab.id}`} role="tab" aria-controls={tab.id} aria-selected="true">{tab.name}</a>
+                        <li key={index} className={`nav-item pos-relative ${tab.active ? 'active' : ''}`}>
+                            <a className={`nav-link bordered-tab ${tab.active ? 'active' : ''}`} id={`${tab.id}-tab`} data-toggle="tab" href={`#${tab.id}`} role="tab" aria-controls={tab.id} aria-selected="true">{tab.name} 
+                            </a><i onClick={ () => this.removeTab(tab.id)} className="fa tab-close-btn fa-close pull-right"></i>
                         </li>
                     ))}
                     <li className="nav-item">
-                        <a className="nav-link bordered-tab" href="#"><i className="fa fa-plus"></i></a>
+                        <a className="nav-link bordered-tab link-cursor" onClick={this.newTab}><i className="fa fa-plus"></i></a>
                     </li>
                 </ul>
                 <div className="tab-content" id="myTabContent">
