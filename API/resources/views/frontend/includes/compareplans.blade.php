c932caba2113485a1299360193db13d7fb706b93
joneslee0918 <joneslee0918@gmail.com>
2019-07-11 17:49:52 +0200

    some fixes

--- API/resources/views/frontend/includes/compareplans.blade.php
+++ API/resources/views/frontend/includes/compareplans.blade.php
@@ -3,7 +3,7 @@
         <div class="row mt30 mb30">
             <div class="col-xs-12 text-center">
                 <h2>Try UniClix and see the difference.</h2>
-                <a href="{{ route('pricing') }}" class="signin-btn">Compare Plans</a>
+                <a href="{{ route('pricing') }}" class="btn signin-btn get-started-btn">Compare Plans</a>
             </div>
         </div>
     </div>
