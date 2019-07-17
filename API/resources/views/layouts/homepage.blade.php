67fc38ab9f875eec40714ec51e738752c6e3c911
joneslee0918 <joneslee0918@gmail.com>
2019-07-17 15:07:39 +0200

    robot indexing environment variable

--- API/resources/views/layouts/homepage.blade.php
+++ API/resources/views/layouts/homepage.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="index, follow">
+    <meta name="robots" content="{{config('seo-indexing.value')}}">
     <title>Uniclix</title>
 
     <!-- Scripts -->
